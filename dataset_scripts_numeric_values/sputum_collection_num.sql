c936a457e6f0f236143221803d6b994f9aeede98
madrine nalukwago <mnalukwago26@gmail.com>
2026-02-05 16:05:05 +0300

    modified sputum collection script

--- dataset_scripts_numeric_values/sputum_collection_num.sql
+++ dataset_scripts_numeric_values/sputum_collection_num.sql
@@ -18,42 +18,42 @@ INSERT INTO cht.fact_cht_numeric_values (
     source_form
 )
 SELECT
-    doc_id AS uuid,
+   uuid,
     'Tuberclosis' AS theme,
     'sputum_collection' AS dataset,
     unpivot.data_element,
     unpivot.value,
     date,
     chw_id,
-    facility_id,
-    district AS district_id,
-    region,
+    contact_facility_id as facility_id,
+    district_id,
+    region_id as region,
     patient_age_in_years,
     patient_age_in_months,
     patient_age_in_days,
     patient_gender AS patient_sex,
-    contact_date_of_birth AS patient_dob,
+    n_contact_date_of_birth AS patient_dob,
     'cht' AS source_system,
     'sputum_collection' AS source_form
 FROM (
     SELECT
-        doc_id,
+        uuid,
         date,
         chw_id,
-        facility_id,
-        district,
-        region,
+        contact_facility_id,
+        district_id,
+        region_id,
         patient_age_in_years,
         patient_age_in_months,
         patient_age_in_days,
         patient_gender,
-        contact_date_of_birth,
+        n_contact_date_of_birth,
 
         -- indicator columns
         LOWER(TRIM(is_patient_available)) AS is_patient_available,
         LOWER(TRIM(has_produced_sputum)) AS has_produced_sputum,
         LOWER(TRIM(confirm_send_sample_for_testing)) AS confirm_send_sample_for_testing
-    FROM report.mv_sputum_collection
+    FROM cht.mv_sputum_collection
 ) src
 CROSS JOIN LATERAL (
     VALUES
